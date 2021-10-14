821c7ceec4584d1557d534d4ded61baf19f5bcbf
lilciv <92471573+lilciv@users.noreply.github.com>
2021-10-14 12:13:02 -0500

    Update RustServerInstaller.bat

--- RustServerInstaller.bat
+++ RustServerInstaller.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Rust Server Installer (v1.0.6) by lilciv#2944
+REM Rust Server Installer (v1.0.7) by lilciv#2944
 color 02
 mode 110,20
 :steamcmd
@@ -19,8 +19,11 @@ echo.
 :branch
 title Setting Up Your Server
 echo.
-echo Note: There is no direct Oxide support for the Staging Branch. You will have to manually install it.
+echo.
+echo Note: There is direct Oxide support for the Staging Branch. You will have to manually install it.
+echo.
 choice /c yn /m "Do you want to install a Staging Branch server? (Usually, you DO NOT want to do this): "
+echo.
 If ERRORLEVEL 2 goto rustmain
 IF ERRORLEVEL 1 goto ruststaging
 
@@ -72,13 +75,13 @@ echo echo Rust Staging Updated! >> UpdateServer.bat
 echo pause>> UpdateServer.bat
 echo Rust Staging installed!
 echo.
-goto startup
+goto map
 
 :choice
 title Setting Up Your Server
 choice /c yn /m "Would you like to install Oxide?: "
 echo.
-IF ERRORLEVEL 2 goto startup
+IF ERRORLEVEL 2 goto map
 IF ERRORLEVEL 1 goto oxide
 
 :oxide
@@ -98,8 +101,16 @@ echo.
 echo Oxide installed!
 echo.
 echo.
+goto map
 
-:startup
+:map
+title Setting Up Your Server
+choice /c yn /m "Are you using a custom map file? (For normal/first time installs, you likely aren't): "
+echo.
+IF ERRORLEVEL 2 goto startproc
+IF ERRORLEVEL 1 goto startcustom
+
+:startproc
 cd /d %forceinstall%
 title Creating Your Startup File...
 set serverport=28015
@@ -150,7 +161,57 @@ echo +rcon.port %rconport% ^^>> StartServer.bat
 echo +rcon.password %rconpw% ^^>> StartServer.bat
 echo +rcon.web 1 ^^>> StartServer.bat
 echo goto start>> StartServer.bat
+goto finish
 
+:startcustom
+cd /d %forceinstall%
+title Creating Your Startup File (Custom Map)...
+set serverport=28015
+set /p serverport="Enter your server port (Default: 28015): "
+echo.
+set rconport=28016
+set /p rconport="Enter your RCON port (Default: 28016): "
+echo.
+set levelurl=https://www.dropbox.com/s/xyprhdhq5l8tmsf/procrustedit.map?dl=1
+set /p levelurl="Enter your custom map map URL (must be a direct download link!): "
+echo.
+set maxplayers=150
+set /p maxplayers="Enter the max players (Default: 150): "
+echo.
+set hostname=Dedicated Rust Server
+set /p hostname="Enter your server's hostname (How it appears on the server browser): "
+echo.
+set description=An unconfigured Rust server.
+set /p description="Enter your server's description: "
+echo.
+set rconpw=ChangeThisPlease
+set /p rconpw="Enter your RCON password (make it secure!): "
+echo.
+set /p serverurl="Enter your Server URL (Ex: Your Discord invite link. Can be blank if you don't have one): "
+echo.
+echo Reminder: Your Header Image MUST contain ONLY the picture, at 1024x512 size (Ex: https://i.imgur.com/5CFzQHl.png)
+set /p headerimage="Enter your Server Header Image (If you don't have one, leave this blank): "
+
+echo @echo off> StartServer.bat
+echo :start>> StartServer.bat
+echo RustDedicated.exe -batchmode ^^>> StartServer.bat
+echo -logFile "ServerLogs.txt" ^^>>StartServer.bat
+echo -silent-crashes ^^>>StartServer.bat
+echo -levelurl "%levelurl%" ^^>> StartServer.bat
+echo +server.port %serverport% ^^>> StartServer.bat
+echo +server.maxplayers %maxplayers% ^^>> StartServer.bat
+echo +server.hostname "%hostname%" ^^>> StartServer.bat
+echo +server.description "%description%" ^^>> StartServer.bat
+echo +server.headerimage "%headerimage%" ^^>>StartServer.bat
+echo +server.url "%serverurl%" ^^>>StartServer.bat
+echo +server.identity "RustServer" ^^>> StartServer.bat
+echo +rcon.port %rconport% ^^>> StartServer.bat
+echo +rcon.password %rconpw% ^^>> StartServer.bat
+echo +rcon.web 1 ^^>> StartServer.bat
+echo goto start>> StartServer.bat
+goto finish
+
+:finish
 echo.
 echo.
 echo.
